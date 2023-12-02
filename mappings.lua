0110d52d9c048e4098b76bc16ff542b014fb81db
Léo Coletta <mail.leo.coletta@gmail.com>
2023-12-02 14:14:02 +0100

    feat: Add mapping to restart LSPs

--- mappings.lua
+++ mappings.lua
@@ -38,8 +38,11 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+
+    ["F9"] = { ":LspRestart", desc = "Restart LSPs" },
   },
 
   -- Insert mode
--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -16,6 +16,7 @@ return {
       "tsx",
       "vue",
       "svelte",
+      "prisma",
     })
 
     opts.compilers = { "zig" }
