5f1e8835024cedfafb8d1f226f5e4506d42ad6fa
Léo Coletta <mail.leo.coletta@gmail.com>
2024-04-01 13:53:47 +0200

    feat: Change theme

--- plugins/community.lua
+++ plugins/community.lua
@@ -2,4 +2,5 @@ return {
   "AstroNvim/astrocommunity",
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   { import = "astrocommunity.completion.copilot-lua" },
+  -- { import = "astrocommunity.color.transparent-nvim" },
 }
