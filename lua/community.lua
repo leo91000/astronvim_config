750f67fec6ec70a3ed192bf15db5eff08c979e53
Léo Coletta <mail.leo.coletta@gmail.com>
2024-08-21 11:23:24 +0200

    feat: Add rust community plugin

--- lua/community.lua
+++ lua/community.lua
@@ -1,6 +1,7 @@
 ---@type LazySpec
 return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.completion.copilot-lua" },
+	"AstroNvim/astrocommunity",
+	{ import = "astrocommunity.pack.lua" },
+	{ import = "astrocommunity.pack.rust" },
+	{ import = "astrocommunity.completion.copilot-lua" },
 }
